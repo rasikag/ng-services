3cb1dffd72b24cf9eb9543525d857adc78888efd
rasikag <rasikagayan90@gmail.com>
2018-09-23 22:14:10 +0800

    How many Instances of Service Should It Be

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,12 +6,9 @@ import { AccountService } from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService]
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
- 
-  
-  
 
   constructor(private loggingService: LoggingService,
               private accountService: AccountService) {
