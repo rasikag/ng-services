18ea2bd0a4c4f0caa7e908ee0a9001c8b7ed5ed9
rasikag <rasikagayan90@gmail.com>
2018-09-23 15:52:19 +0800

    Injecting the Logging Service into Components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -18,6 +18,6 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange('A server status changed, new status: ' + status);
   }
 }
